8113abb3ff81e6a764f2edd741c7cb29efd3a115
SeungJun Hwang <s-eungju-n@hanmail.net>
2019-03-14 11:15:14 +0900

    test1

---
+++ week2_1/main.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(string arrangement) {
+	int answer = 0;
+	cout << arrangement << endl;
+	return answer;
+}
+
+void main()
+{
+	string ans = "()(((()())(())()))(())";
+	solution(ans);
+}
