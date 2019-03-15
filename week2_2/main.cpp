20895483339dca7bceb8262486dba5bfc1520d82
SeungJun Hwang <s-eungju-n@hanmail.net>
2019-03-15 18:50:06 +0900

    week2-2

---
+++ week2_2/main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+	int answer = 0;
+	queue<int> que; 
+
+	for (auto elem : priorities)
+	{
+		que.push(elem);
+	}
+
+	return answer;
+}
+
+void main()
+{
+
+}
