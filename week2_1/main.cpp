df52154167f19b2e8fb5dec0141b55dc13c109d1
SeungJun Hwang <s-eungju-n@hanmail.net>
2019-03-15 19:01:08 +0900

    2_1 commit

--- week2_1/main.cpp
+++ week2_1/main.cpp
@@ -1,17 +1,51 @@
 #include <string>
 #include <vector>
 #include <iostream>
-
+#include <queue>
 using namespace std;
 
 int solution(string arrangement) {
 	int answer = 0;
-	cout << arrangement << endl;
+	queue<char> queue;
+
+	int stick = 0;
+	for (auto elem : arrangement)
+	{
+		if (queue.size() > 0)
+		{
+			if (queue.back() == '(' && elem == '(')
+			{
+				stick += 1;
+				queue.pop();
+				queue.push(elem);
+			}
+			else if (queue.back() == '(' && elem == ')')
+			{
+				answer += stick;
+				queue.pop();
+			}
+		}
+		else
+		{
+			if (elem == ')')
+			{
+				stick -= 1;
+				answer += 1;
+			}
+			else
+			{
+				queue.push(elem);
+			}
+		}
+	}
+
 	return answer;
 }
 
 void main()
 {
 	string ans = "()(((()())(())()))(())";
+
+	
 	solution(ans);
 }
